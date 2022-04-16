93c11d06cd2ac9f2f3fb11f04c95372975cef4cb
ZackTheAwkward <zack.e.gaddy@gmail.com>
2022-04-15 19:21:41 -0600

    Spring framework config

---
+++ LocalBreweries/src/main/java/com/skilldistillery/breweries/LocalBreweriesApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.breweries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalBreweriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalBreweriesApplication.class, args);
+	}
+
+}
---
+++ LocalBreweries/src/test/java/com/skilldistillery/breweries/LocalBreweriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.breweries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalBreweriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
